c495470f2a52f9ca6ebdbc565d24d0d7d4b40005
unknown <hadeel.f.hazimeh@gmail.com>
2024-01-05 21:15:40 +0300

    BE:register function

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,20 +4,61 @@ const jwt = require("jsonwebtoken");
 
 // This function creates a new user (doctor or patient)
 //register
-const register=(req, res)=>{
-
-
-  
-}
-
+const register = (req, res) => {
+  const {
+    name,
+    age,
+    email,
+    password,
+    phoneNumber,
+    specialist,
+    appointments,
+    role,
+    patientHistory,
+  } = req.body;
+  const newUser = new usersModel({
+    name,
+    age,
+    email,
+    password,
+    phoneNumber,
+    specialist/*specialist ? specialist : ""*/,
+    appointments,
+    role,
+    patientHistory,
+  });
+  newUser
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: "Account Created Successfully",
+        user: result,
+      });
+     
+      console.log(result);
+    })
+    .catch((err) => {
+      if (err.keyPattern) {
+        return res.status(409).json({
+          success: false,
+          message: "The email already exists",
+        });
+      }
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        Error: err.message,
+      });
+    });
+   
+};
 
 //login function checks user login credentials
- 
-const login=(req, res)=>{}
 
+const login = (req, res) => {};
 
- module.exports = {
-    register,
-    login,
-  };
-  
+module.exports = {
+  register,
+  login,
+};
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
+const bcrypt = require("bcryptjs");
 const userSchema=({
-    Name: { type: String },
+    name: { type: String },
     age: { type: Number },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -9,6 +10,6 @@ const userSchema=({
     appointments:[{ type: mongoose.Schema.Types.ObjectId, ref: "Appointments"}],
     role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
     patientHistory:[{type: mongoose.Schema.Types.ObjectId, ref: "PatientCase"}]
-})
+});
 //pre middleware
 module.exports=mongoose.model("User",userSchema)
