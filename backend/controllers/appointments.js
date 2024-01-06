7dbb77bb2c775481875501d95ac36df21271aea0
unknown <hadeel.f.hazimeh@gmail.com>
2024-01-06 15:05:25 +0300

    BE:bookAppointment function

--- backend/controllers/appointments.js
+++ backend/controllers/appointments.js
@@ -4,12 +4,12 @@ const appointmentsModel = require("../models/appointment");
 const bookAppointment=(req,res)=>{
 const {patient,doctor,date,hour,clinic}=req.body;
 // const specificDate = new Date('2024-01-06T12:00:00Z');
-const newAppointment= new appointmentsModel({patient,doctor,date,hour,clinic,isBooked:true});
+const newAppointment= new appointmentsModel({patient,doctor,date,clinic,isBooked:true});
 
-const check= appointmentsModel.find({isBooked:false});
-console.log("check",check);
+const check= appointmentsModel.find({isBooked:false});// if true the appointment is available
+ console.log("check:",check);
+
 //check if the appointemnt is reserved 
-
 if (!check){
     return res.status(409).json({
         success: false,
@@ -28,13 +28,6 @@ newAppointment.save()
 
   })
   .catch((err) => {
-    // console.log("err",err)
-    // if (err.keyPattern) {
-    //   return res.status(409).json({
-    //     success: false,
-    //     message: "The appointment has been already reserved,please choose anthor time",
-    //   });
-    // }
     res.status(500).json({
       success: false,
       message: `Server Error`,
@@ -46,7 +39,7 @@ newAppointment.save()
 
 
 }
-
+ const 
 
 module.exports={
 bookAppointment,
