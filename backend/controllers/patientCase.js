6872068f5aada9d52c8dbd5ef515373b26e274d8
unknown <hadeel.f.hazimeh@gmail.com>
2024-01-08 16:40:01 +0300

    BE :updateHistoryById function

--- backend/controllers/patientCase.js
+++ backend/controllers/patientCase.js
@@ -38,7 +38,7 @@ const getPatientHistory = (req, res) => {
   const { patientId } = req.params;
   console.log(patientId);
   patient
-    .findOne({ _id: patientId }).populate("patientHistory name")
+    .findOne({ _id: patientId }).populate("patientHistory")
     .then((result) => {
       console.log("result", result);
       if (!result) {
@@ -51,7 +51,7 @@ const getPatientHistory = (req, res) => {
       res.json({
         success: true,
         message: `The patient History ${patientId}`,
-        patientHistory: result,
+        patientHistory: result.patientHistory,
       });
     })
 
@@ -65,9 +65,65 @@ const getPatientHistory = (req, res) => {
     });
 };
 
-const updateHistoryById = () => {};
+// const updateHistoryById = (req,res) => {
+// const {historyId}=req.params;
+// const filter={_id:historyId};
+// //const update={diagnosis:"stomache",treatment:"fomodar"};
+// const update=req.body;
+// patient.findOneAndUpdate(filter,update).then((result)=>{
+//   res.status(201).json({
+//     success: true,
+//     message: "patient treatment or diagnosis updated",
+//     patientCase:result
+//   });
+// }).catch((err)=>{
+//   res.status(500).json({
+//     success: false,
+//     message: `Server Error`,
+//     Error: err.message,
+//   });
+// })
+const updateHistoryById = (req, res) => {
+  const {historyId} = req.params;
+  const filter = req.body;
+  console.log(filter)
+  Object.keys(filter).forEach((key) => {
+    filter[key].toString().replaceAll(" ", "") == "" && delete filter[key];
+  });
+  patientCase
+    .findOneAndUpdate({ _id: historyId }, req.body, { new: true })
+    .then((result) => {
+      console.log("Result",result)
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The patient History with id => ${historyId} not found`,
+        });
+      }
+      res.status(202).json({
+        success: true,
+        message: `patient History  updated`,
+        upatedpatientCase:result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
+
+
+
+
+
+
 
 module.exports = {
   createPatientCase,
   getPatientHistory,
+  updateHistoryById
 };
--- backend/routes/patientCases.js
+++ backend/routes/patientCases.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const authentication=require("../middleware/authentication");
 const authorization=require("../middleware/authorization");
-const {createPatientCase,getPatientHistory} = require("../controllers/patientCase");
+const {createPatientCase,getPatientHistory,updateHistoryById} = require("../controllers/patientCase");
 const patientCaseRouter = express.Router();
 
 patientCaseRouter.post("/create/:patientId",authentication,authorization("Create"),createPatientCase)
 patientCaseRouter.get("/get/:patientId",getPatientHistory)
+patientCaseRouter.put("/update/:historyId",updateHistoryById)
 module.exports = patientCaseRouter;
 
