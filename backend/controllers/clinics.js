c521752887490a8de4b01e7705364d3ce6f07d47
unknown <hadeel.f.hazimeh@gmail.com>
2024-01-09 18:10:27 +0300

    BE:get Clincics functions

--- backend/controllers/clinics.js
+++ backend/controllers/clinics.js
@@ -1,8 +1,103 @@
-const clinic = require("../models/clinic")
+const clinicModel = require("../models/clinic")
 
+//in this function new docotor can be added to the clinic adding name and image
 const createClinic=(req,res)=>{
-
+    const {clinicName,reviews} = req.body;
+    const newClinic = new clinicModel({clinicName,reviews});
+    console.log(newClinic)
+   newClinic
+      .save()
+      .then((result) => {
+        console.log("result",result)
+        res.status(201).json({
+          success: true,
+          message: "Clinic created",
+          clinic: result,
+      });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: `Server Error`,
+          Error: err.message,
+        });
+      });
 }
+
+const addnewDoctor=(req,res)=>{
+//add the doctor by clinic id 
+
+
+    const {clinicId }= req.params;
+    const { doctors,image } = req.body;
+    const docotors = req.token.userId;
+    const newDoctor = new clinicModel({
+      doctors,
+      image,
+    });
+    newDoctor
+      .save()
+      .then((result) => {
+        articlesModel
+          .findByIdAndUpdate(
+            { _id: clinicId },
+            { $push: { Doctors: result } },
+            { new: true }
+          )
+          .then(() => {
+            res.status(201).json({
+              success: true,
+              message: `Comment added`,
+              comment: result,
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              success: false,
+              message: `Server Error`,
+              err: err.message,
+            });
+          });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: `Server Error`,
+          err: err.message,
+        });
+      });
+  };
+
+
+const getAllClinics=(req,res)=>
+{       const userId = req.token.userId;
+        clinicModel
+          .find()
+          .populate("clinicName")
+          .exec()
+          .then((clinics) => {
+            if (clinics.length) {
+              res.status(200).json({
+                success: true,
+                message: `All the clinics`,
+                userId: userId,
+                clinics: clinics,
+              });
+            } else {
+              res.status(200).json({
+                success: false,
+                message: `No clinics Yet`,
+              });
+            }
+          })
+          .catch((err) => {
+            res.status(500).json({
+              success: false,
+              message: `Server Error`,
+              err: err.message,
+            });
+          });
+      };
 
 
 
@@ -10,5 +105,6 @@ const createClinic=(req,res)=>{
 
 
 module.exports = {
-    createClinic
+    createClinic,
+    getAllClinics
   };
--- backend/routes/clinic.js
+++ backend/routes/clinic.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authentication=require("../middleware/authentication");
 const authorization=require("../middleware/authorization");
-const {createClinic} = require("../controllers/clinics");
+const {createClinic, getAllClinics} = require("../controllers/clinics");
 const clinicRouter = express.Router();
 
-clinicRouter.post("/create",createClinic)
+clinicRouter.post("/create",createClinic);
+clinicRouter.get("/get",authentication,getAllClinics);
+
 
 module.exports =clinicRouter;
