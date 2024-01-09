aeef298c1a4257977d34e1fc6aaf281e0f76db26
unknown <hadeel.f.hazimeh@gmail.com>
2024-01-09 21:14:25 +0300

    BE:  render clinic and doctors functions

--- backend/controllers/clinics.js
+++ backend/controllers/clinics.js
@@ -1,123 +1,150 @@
-const clinicModel = require("../models/clinic")
+const clinicModel = require("../models/clinic");
 
-//in this function new docotor can be added to the clinic adding name and image
-const addnewDoctor=async(req,res)=>{
-    const {clinicName,image} = req.body;
-    const doctor = req.token.userId;
-    const newDoctor = new clinicModel({clinicName,image,doctor});
-    //check if the doctor is already registered at the same clinic
+//this function is to render the clinic names
+const getClinicNames = (req, res) => {
+  const clinicNames = ["Derma", "Dental", "General"];
+  return res.status(200).json({
+    success: true,
+    clinics: clinicNames,
+  });
+};
 
-    const checkDoctor= await clinicModel.findOne({ doctor: doctor,
-        clinicName:clinicName});
+//in this function new docotor can be added to the clinic adding name and image to sepcific clinic names
+//clinicName=["Derma","Dental","general"]
 
-      console.log("Doctor:",checkDoctor)
-    
-     if (checkDoctor)
-     {
-      return res.status(400).json({
+const addnewDoctor = async (req, res) => {
+  const { clinicName, image } = req.body;
+  const doctor = req.token.userId;
+  const newDoctor = new clinicModel({ clinicName, image, doctor });
+  //check if the doctor is already registered at the same clinic
+
+  const checkDoctor = await clinicModel.findOne({
+    doctor: doctor,
+    clinicName: clinicName,
+  });
+
+  //   console.log("Doctor:",checkDoctor)
+
+  if (checkDoctor) {
+    return res.status(400).json({
+      success: false,
+      message: "the doctor is already registered at this clinic ",
+    });
+  }
+  newDoctor
+    .save()
+    .then((result) => {
+      console.log("result", result);
+      res.status(201).json({
+        success: true,
+        message: "doctor Added successfully",
+        clinicInfo: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
         success: false,
-        message: "the doctor is already registered at this clinic ",
+        message: `Server Error`,
+        Error: err.message,
       });
-     }
-   newDoctor
-      .save()
-      .then((result) => {
-        console.log("result",result)
-        res.status(201).json({
+    });
+};
+const getAllClinics = (req, res) => {
+  const userId = req.token.userId;
+  clinicModel
+    .find()
+    .populate("clinicName")
+    .exec()
+    .then((result) => {
+      if (result.length) {
+        res.status(200).json({
           success: true,
-          message: "doctor Added successfully",
-          clinicInfo: result,
-      });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: `Server Error`,
-          Error: err.message,
+          message: "All the Information",
+          userId: userId,
+          doctorInfo: result,
         });
+      } else {
+        res.status(200).json({
+          success: false,
+          message: `No doctors added to the clinic yet`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
       });
-}
+    });
+};
 
-// const addnewDoctor=(req,res)=>{
-// //add the doctor by clinic id 
-//     const {clinicId }= req.params;
-//     const {image } = req.body;
-//     const doctor = req.token.userId;
-//     const newDoctor = new clinicModel({
-//       doctors:doctor,
-//       image,
-//     });
-//     newDoctor
-//       .save()
-//       .then((result) => {
-//         clinicModel
-//           .findByIdAndUpdate(
-//             { _id: clinicId },
-//             { $push: { doctors: result } },
-//             { new: true }
-//           )
-//           .then(() => {
-//             res.status(201).json({
-//               success: true,
-//               message: `Comment added`,
-//               comment: result,
-//             });
-//           })
-//           .catch((err) => {
-//             res.status(500).json({
-//               success: false,
-//               message: `Server Error`,
-//               err: err.message,
-//             });
-//           });
-//       })
-//       .catch((err) => {
-//         res.status(500).json({
-//           success: false,
-//           message: `Server Error`,
-//           err: err.message,
-//         });
-//       });
-//   };
+//the patient can render the doctors info
+const getByDoctorId = (req, res) => {
+  const userId = req.token.userId;
+  const { doctorId } = req.params;
+  clinicModel
+    .find({ doctor: doctorId })
+    .populate("doctor", "name")
+    .exec()
+    .then((result) => {
+      if (result.length) {
+        res.status(200).json({
+          success: true,
+          message: "",
+          userId: userId,
+          doctorInfo: result,
+        });
+      } else {
+        res.status(200).json({
+          success: false,
+          message: `No doctors added yet`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
 
-
-const getAllClinics=(req,res)=>
-{       const userId = req.token.userId;
-        clinicModel
-          .find()
-          .populate("clinicName")
-          .exec()
-          .then((clinics) => {
-            if (clinics.length) {
-              res.status(200).json({
-                success: true,
-                message: `All the clinics`,
-                userId: userId,
-                clinics: clinics,
-              });
-            } else {
-              res.status(200).json({
-                success: false,
-                message: `No clinics Yet`,
-              });
-            }
-          })
-          .catch((err) => {
-            res.status(500).json({
-              success: false,
-              message: `Server Error`,
-              err: err.message,
-            });
-          });
-      };
-
-//getByClinicName
-//get By Dr Id
 //delete by DrId
+const deleteDoctorById = (req,res) => {
+  const doctor= req.token.userId;
+  console.log(doctor)
+  clinicModel
+  .findOneAndDelete(doctor)
+  .then((result) => {
+  console.log(result)
+    if (!result) {
+    return res.status(404).json({
+    success: false,
+   message: "The doctor is not found",
+      });
+      }
+      res.status(200).json({
+        success: true,
+        message: "doctor Info deleted",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
 //create review
 
-
 module.exports = {
-    addnewDoctor,
-    getAllClinics
-  };
+  addnewDoctor,
+  getAllClinics,
+  getClinicNames,
+  getByDoctorId,
+  deleteDoctorById
+};
--- backend/routes/clinic.js
+++ backend/routes/clinic.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authentication=require("../middleware/authentication");
 const authorization=require("../middleware/authorization");
-const {addnewDoctor, getAllClinics} = require("../controllers/clinics");
+const {addnewDoctor, getAllClinics,getClinicNames,getByDoctorId,deleteDoctorById} = require("../controllers/clinics");
 const clinicRouter = express.Router();
-
+clinicRouter.get("/clinicNames",getClinicNames)
 clinicRouter.post("/create",authentication,authorization("Create"),addnewDoctor);
 clinicRouter.get("/get",authentication,getAllClinics);
+clinicRouter.get("/get/:doctorId",authentication,getByDoctorId)
 
-
+clinicRouter.delete("/delete",authentication,deleteDoctorById)
 module.exports =clinicRouter;
