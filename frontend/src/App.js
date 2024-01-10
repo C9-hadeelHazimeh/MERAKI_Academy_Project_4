8f0835b968458a5c22880332330a7f8994da4069
unknown <hadeel.f.hazimeh@gmail.com>
2024-01-10 12:29:16 +0300

    FE: Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
-
+import Home from "./components/Home";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
  
   return (
     <div className="App">
       <h1>Hello, World</h1>
-      <Login/>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      {/* <Home/> */}
+      </Routes> 
     </div>
   );
 }
